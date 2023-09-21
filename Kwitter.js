437216671b8389020cf5520d61f57dcbd6250fc1
DarkSorriso <123620578+DarkSorriso@users.noreply.github.com>
2023-09-21 18:23:24 -0300

    Add files via upload

---
+++ Kwitter.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyD6TqGDXIfEtaXc3A--T8o4V0J8HiWPM1I",
+    authDomain: "dever-8ecc5.firebaseapp.com",
+    databaseURL: "https://dever-8ecc5-default-rtdb.firebaseio.com",
+    projectId: "dever-8ecc5",
+    storageBucket: "dever-8ecc5.appspot.com",
+    messagingSenderId: "506006628790",
+    appId: "1:506006628790:web:97cacc64db4c7a8b543917"
+  };
+
+  firebase.initializeApp(firebaseConfig);
